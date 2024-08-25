d864c4fcfb42acf361ad31c2863c93c18c038b37
Rhitwik <chitwik@gmail.com>
2024-08-25 20:16:32 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
                             <p>city Guwahati, VIP road</p>
                         </div>
                         <div className="d-flex">
-                            <a href="tel:555-555-555">+91 7099028155</a>
+                            <a href="tel:+917099028155">+91 7099028155</a>
                         </div>
                         <div className="d-flex">
                             <p>rhitwik3@gmail.com</p>
@@ -27,18 +27,18 @@ const Footer = () => {
                     <div className="col-lg-3 col-md-2 col-sm-6">
                         <div className="row">
                             <div className="col">
-                                <a className="footer-nav">Home</a>
+                                <a href="/home" className="footer-nav">Home</a>
                                 <br/>
-                                <a className="footer-nav">About Me</a>
+                                <a href="/about" className="footer-nav">About Me</a>
                                 <br/>
-                                <a className="footer-nav">Portfolio</a>
+                                <a href="/portfolio" className="footer-nav">Portfolio</a>
                             </div>
                             <div className="col">
-                                <a className="footer-nav">Experience</a>
+                                <a href="/experience" className="footer-nav">Experience</a>
                                 <br/>
-                                <a className="footer-nav">Skills</a>
+                                <a href="/skills" className="footer-nav">Skills</a>
                                 <br/>
-                                <a className="footer-nav">Contacts</a>
+                                <a href="/contacts" className="footer-nav">Contacts</a>
                             </div>
                         </div>
                     </div>
@@ -51,20 +51,6 @@ const Footer = () => {
                             >
                                 <FacebookIcon className="mx-3" size={36}/>
                             </FacebookShareButton>
-                            {/* <TwitterShareButton
-                                url={"https://x.com/Rhitwik003"}
-                                quote={"FullStack Developer"}
-                                hashtag="#javascript"
-                            >
-                                <TwitterIcon className="mx-3" size={36}/>
-                            </TwitterShareButton>
-                            <LineShareButton
-                                url={"https://www.linkedin.com/in/rhitwik-choudhury-509037226/"}
-                                quote={"FullStack Developer"}
-                                hashtag="#javascript"
-                            >
-                                <LinkedinIcon className="mx-3" size={36}/>
-                            </LineShareButton> */}
                             <TwitterShareButton
                                 url={"https://x.com/Rhitwik003"}
                             >
